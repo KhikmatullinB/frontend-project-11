53e9b5fbd7670e53b510320c7b7779a7ef79c409
KhikmatullinB <colonspawn@gmail.com>
2024-05-17 19:02:04 +0800

    installed bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,12 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export default {
+  mode: process.env.NODE_ENV || 'development',
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    filename: '[name].[contenthash].js',
+    filename: './src/index.js',
     path: path.resolve(__dirname, './dist/'),
     clean: true,
   },
@@ -16,4 +18,16 @@ export default {
       template: path.resolve(__dirname, 'index.html'),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
 };
