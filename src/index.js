a23296b5a61e0f752998fbe981945e14b34a652e
KhikmatullinB <colonspawn@gmail.com>
2024-05-18 15:28:23 +0800

    deleted test scripts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import { data } from './data.js';
 import '../styles/style.scss';
 import 'bootstrap';
-
-const root = document.getElementById('app');
-
-data.forEach((element) => {
-  const item = document.createElement('div');
-  item.textContent = element;
-  item.classList.add('item');
-  root.appendChild(item);
-});
