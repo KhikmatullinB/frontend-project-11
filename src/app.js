54f95cc86a9e492591bd35c2efc753b21d30e76e
KhikmatullinB <colonspawn@gmail.com>
2024-05-21 20:51:16 +0800

    added states

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
 import * as yup from 'yup';
+
+const runApp = () => {
+  const initialState = {
+    form: {
+      processState: 'filling',
+      errors: '',
+    },
+    feeds: [],
+    posts: [],
+    visitedPostsId: new Set(),
+    currentPostId: '',
+  };
+
+const elements = {
+    former: document.querySelector('form'),
+    input: document.querySelector('input'),
+    button: document.querySelector('button'),
+    
+}
+}
