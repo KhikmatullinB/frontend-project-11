9d6aff7f913f89db8dc59bfbcfce10889cd97f08
KhikmatullinB <colonspawn@gmail.com>
2024-05-23 21:01:15 +0800

    created handleProcess

--- src/view.js
+++ src/view.js
@@ -1,1 +1,15 @@
 import onChange from 'on-change';
+
+const handleProcessState = (elements, i18nextInstance, processState) => {
+  switch (processState) {
+    case 'filling':
+      elements.submit.disabled = false;
+      break;
+    case 'sending':
+      elements.submit.disabled = true;
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.add('text-success');
+      elements.input.classList.remove('is-invalid');
+      elements.feedback.textContent = i18nextInstance.t('process.download');
+  }
+};
