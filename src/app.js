87c558619b926ef043783d32448f1506981c293b
KhikmatullinB <colonspawn@gmail.com>
2024-05-23 21:00:40 +0800

    created i18next instance

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 import * as yup from 'yup';
+import i18next from 'i18next';
+import resources from '../locales/ru.js';
 
 const runApp = () => {
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  });
+
   const initialState = {
     form: {
       processState: 'filling',
@@ -12,10 +21,30 @@ const runApp = () => {
     currentPostId: '',
   };
 
-const elements = {
-    former: document.querySelector('form'),
-    input: document.querySelector('input'),
-    button: document.querySelector('button'),
-    
-}
-}
+  yup.setLocale({
+    string: {
+      default: 'NotValidUrl',
+    },
+    mixed: {
+      notOneOf: 'notOneOf',
+    },
+  });
+
+  const input = document.querySelector('#url-input');
+  const form = document.querySelector('.rss-form');
+  const feedback = document.querySelector('.feedback');
+  const submit = document.querySelector('button[type="submit"]');
+  const feeds = document.querySelector('.feeds');
+  const posts = document.querySelector('.posts');
+  const modal = document.querySelector('#modal');
+
+  const elements = {
+    input,
+    form,
+    feedback,
+    submit,
+    feeds,
+    posts,
+    modal,
+  };
+};
