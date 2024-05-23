8f3b5a176696d2f7098216d20d8fca45c7dcac34
KhikmatullinB <colonspawn@gmail.com>
2024-05-23 20:59:46 +0800

    created dir with i18next config

---
+++ locales/ru.js
@@ -0,0 +1,13 @@
+export default {
+  translation: {
+    errors: {
+      notValidUrl: 'Ссылка должна быть валидным URL',
+    },
+    process: {
+      download: 'Идет загрузка',
+    },
+    success: {
+      rss: 'RSS успешно загружен',
+    },
+  },
+};
