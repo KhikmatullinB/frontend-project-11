4774c855dc185750200888077fdf32f41f8f83ec
KhikmatullinB <colonspawn@gmail.com>
2024-05-29 18:54:22 +0800

    added render posts

--- src/view.js
+++ src/view.js
@@ -29,50 +29,105 @@ const handleProcessState = (elements, i18nextInstance, processState) => {
     default:
       throw new Error(`Unknown process state: ${processState}`);
   }
+};
 
-  const renderErrors = (elements, i18nextInstance, errorValue) => {
-    if (errorValue === '') {
-      return;
+const renderErrors = (elements, i18nextInstance, errorValue) => {
+  if (errorValue === '') {
+    return;
+  }
+  elements.feedback.classList.remove('text-success');
+  elements.feedback.classList.add('text-danger');
+  elements.input.classList.add('is-invalid');
+
+  elements.feedback.textContent = i18nextInstance.t(`errors.${errorValue}`);
+};
+
+const renderFeeds = (elements, i18nextInstance, feeds) => {
+  elements.feeds.innerHTML = '';
+  const container = document.createElement('div');
+  container.classList.add('card', 'border-0');
+  elements.feeds.append(container);
+
+  const div = document.createElement('div');
+  div.classList.add('card-body');
+  container.append(div);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = i18nextInstance.t('main.feeds');
+  div.append(h2);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  container.append(ul);
+
+  feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = feed.title;
+    li.append(h3);
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+    li.append(p);
+
+    ul.append(li);
+  });
+};
+const renderPosts = (state, elements, i18nextInstance, posts) => {
+  elements.posts.innerHTML = '';
+  const container = document.createElement('div');
+  container.classList.add('card', 'border-0');
+  elements.posts.append(container);
+
+  const div = document.createElement('div');
+  div.classList.add('card-body');
+  container.append(div);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = i18nextInstance.t('main.posts');
+  div.append(h2);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  container.append(ul);
+
+  posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'justify-content-between',
+      'align-items-start',
+      'border-0',
+      'border-end-0'
+    );
+
+    const a = document.createElement('a');
+    if (state.visitedPostsId.has(post.id)) {
+      a.classList.add('fw-normal', 'link-secondary');
+    } else {
+      a.classList.add('fw-bold');
     }
-    elements.feedback.classList.remove('text-success');
-    elements.feedback.classList.add('text-danger');
-    elements.input.classList.add('is-invalid');
+    a.setAttribute('href', post.link);
+    a.setAttribute('data-id', post.id);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = post.title;
+    li.append(a);
 
-    elements.feedback.textContent = i18nextInstance.t(`errors.${errorValue}`);
-  };
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-id', post.id);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+    button.textContent = i18nextInstance.t('main.openModal');
 
-  const renderFeeds = (elements, i18nextInstance, feeds) => {
-    elements.feeds.innerHTML = '';
-    const container = document.createElement('div');
-    container.classList.add('card', 'border-0');
-    elements.feeds.append(container);
-
-    const div = document.createElement('div');
-    div.classList.add('card-body');
-    container.append(div);
-
-    const h2 = document.createElement('h2');
-    h2.classList.add('card-title', 'h4');
-    h2.textContent = i18nextInstance.t('main.feeds');
-    div.append(h2);
-
-    const ul = document.createElement('ul');
-    ul.classList.add('list-group', 'border-0', 'rounded-0');
-    container.append(ul);
-
-    feeds.forEach((feed) => {
-      const li = document.createElement('li');
-      li.classList.add('list-group-item', 'border-0', 'border-end-0');
-      const h3 = document.createElement('h3');
-      h3.classList.add('h6', 'm-0');
-      h3.textContent = feed.title;
-      li.append(h3);
-      const p = document.createElement('p');
-      p.classList.add('m-0', 'small', 'text-black-50');
-      p.textContent = feed.description;
-      li.append(p);
-
-      ul.append(li);
-    });
-  };
+    li.append(button);
+    ul.append(li);
+  });
 };
