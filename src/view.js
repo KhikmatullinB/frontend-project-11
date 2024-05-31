895c94b3fdf08d4bc7c93bde97bedf315eebcada
KhikmatullinB <colonspawn@gmail.com>
2024-05-31 22:55:12 +0800

    fix linter pls

--- src/view.js
+++ src/view.js
@@ -97,14 +97,7 @@ const renderPosts = (state, elements, i18nextInstance, posts) => {
 
   posts.forEach((post) => {
     const li = document.createElement('li');
-    li.classList.add(
-      'list-group-item',
-      'd-flex',
-      'justify-content-between',
-      'align-items-start',
-      'border-0',
-      'border-end-0'
-    );
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
 
     const a = document.createElement('a');
     if (state.visitedPostsId.has(post.id)) {
