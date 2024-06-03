43bcc24ba155846a2cfafda83ffb0a33ed38cab5
KhikmatullinB <colonspawn@gmail.com>
2024-06-03 18:59:33 +0800

    added in src dir

---
+++ src/locales/ru.js
@@ -0,0 +1,22 @@
+export default {
+  translation: {
+    errors: {
+      notValidUrl: 'Ссылка должна быть валидным URL',
+      notOneOf: 'RSS уже существует',
+      notValidRss: 'Ресурс не содержит валидный RSS',
+      network: 'Ошибка сети',
+      unknown: 'Что-то пошло не так... Попробуйте еще раз',
+    },
+    process: {
+      download: 'Идёт загрузка...',
+    },
+    success: {
+      rss: 'RSS успешно загружен',
+    },
+    main: {
+      posts: 'Посты',
+      feeds: 'Фиды',
+      openModal: 'Просмотр',
+    },
+  },
+};
